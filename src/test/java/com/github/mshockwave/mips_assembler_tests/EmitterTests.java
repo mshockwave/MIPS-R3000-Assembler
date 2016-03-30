56db76d4c5a846cfe2f10d5eb62e3450300d10d7
mshockwave <yihshyng223@gmail.com>
2016-03-30 13:48:42 +0800

    Finish first R type instruction test

--- src/test/java/com/github/mshockwave/mips_assembler_tests/EmitterTests.java
+++ src/test/java/com/github/mshockwave/mips_assembler_tests/EmitterTests.java
@@ -37,24 +37,41 @@ public class EmitterTests {
             ParseTreeWalker walker = new ParseTreeWalker();
             walker.walk(emitter, parser.prog());
 
-            /**-----------------Check answer----------------------**/
+            /**-----------------Verify----------------------**/
 
             ByteBuffer buffer = ByteBuffer.wrap(outStream.toByteArray()).order(ByteOrder.BIG_ENDIAN);
             assertEquals("Output byte length", 6 * 4, buffer.array().length);
 
+            //int i;
+            /*
             System.out.println("Raw:");
-            int i;
             byte[] bytes = buffer.array();
             for(i = 0; i < 24; i++){
                 System.out.print(String.format("0x%x ", bytes[i]));
             }
             System.out.println(" ");
+            */
 
+            int v = buffer.getInt(4);
+            assertEquals("Instruction Length", 4, v);
 
+            v = buffer.getInt(8);
+            assertEquals("sub  $3, $2, $1", 0x00411822, v);
+
+            v = buffer.getInt(12);
+            assertEquals("add  $4, $1, $2", 0x00222020, v);
+
+            v = buffer.getInt(16);
+            assertEquals("xor  $4, $3, $4", 0x00642026, v);
+
+            v = buffer.getInt(20);
+            assertEquals("nor  $4, $4, $4", 0x00842027, v);
+            /*
             for(i = 0; i < 6; i++){
                 int v = buffer.getInt(i * 4);
                 System.out.println("0x" + Integer.toHexString(v));
             }
+            */
 
         }catch (IOException e){
             fail("Failed loading resource file");
